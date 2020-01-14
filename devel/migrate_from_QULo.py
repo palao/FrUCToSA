c524c7670a615407c2811227a3c74531aa535b8d
David Palao <david.palao@gmail.com>
2020-01-14 16:11:19 +0100

    WIP: script for migration from QULo

--- devel/migrate_from_QULo.py
+++ devel/migrate_from_QULo.py
@@ -24,10 +24,84 @@
 
 """Script that replaces strings coming from the QULo project and converts them 
 to a convenient form for the FrUCToSA project.
-
-Usage:
-
-python migrate_from_QULo.py my_root_dir
-
 """
 
+import sys
+import argparse
+import os
+from glob import iglob
+import shutil
+
+PATTERNS = ("**/*.py", "**/*.conf")
+
+
+def parse_conf():
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('directories', metavar='DIRs', type=str, nargs='+',
+                        help='space separated list of target input directories')
+    parser.add_argument("-w", '--write', dest='write', action='store_true',
+                        default=False, help='Write back modified files')
+    args = parser.parse_args()
+    return args
+
+def collect_files(conf):
+    files = set()
+    for rootd in conf.directories:
+        for pat in PATTERNS:
+            for e in iglob(os.path.join(rootd, pat), recursive=True):
+                files.add(e)
+    return files
+
+def backup(filename):
+    new_filename = filename+".back"
+    shutil.copy2(filename, new_filename)
+
+def read_file(target_file):
+    with open(target_file) as f:
+        for l in f:
+            yield l
+
+def fix_copyright(lines):
+    for l in lines:
+        if l.startswith("# Copyright (C)"):
+            yield "# Copyright (C) 2020 David Palao\n"
+        else:
+            yield l
+
+def fix_package_name(lines):
+    for l in lines:
+        yield l.replace("QULo", "FrUCToSA")
+        
+def fix_qagent(lines):
+    for l in lines:
+        yield l.replace("qagent", "lagent").replace("QAgent", "LAgent").replace("QAGENT", "LAGENT")
+        
+def fix_qmaster(lines):
+    for l in lines:
+        yield l.replace("qmaster", "lmaster").replace("QMaster", "LMaster").replace("QMASTER", "LMASTER")
+        
+def fix_qulo(lines):
+    for l in lines:
+        yield l.replace("qulo", "fructosa").replace("Qulo", "Fructosa").replace("QULO", "FRUCTOSA")
+
+        
+def process_file(target_file, do_write):
+    if do_write:
+        backup(target_file)
+    raw = read_file(target_file)
+    copyright_fixed = fix_copyright(raw)
+    name_fixed = fix_package_name(copyright_fixed)
+    qagent_replaced = fix_qagent(name_fixed)
+    # for l in raw:
+    #     print(l.strip())
+    
+    
+def main():
+    conf = parse_conf()
+    target_files = collect_files(conf)
+    for target_file in target_files:
+        process_file(target_file, conf.write)
+
+    
+if __name__ == "__main__":
+    sys.exit(main())
