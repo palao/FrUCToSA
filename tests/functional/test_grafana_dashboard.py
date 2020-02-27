2a7aedcebf659ac40f496c34f3329113b6bbb0b0
David Palao <david.palao@gmail.com>
2020-02-27 16:26:09 +0100

    Added placeholder to start FTing new tool to generate Grafana dashboards

---
+++ tests/functional/test_grafana_dashboard.py
@@ -0,0 +1,33 @@
+#!/bin/env python3
+
+#######################################################################
+#
+# Copyright (C) 2020 David Palao
+#
+# This file is part of FrUCToSA.
+#
+#  FrUCToSA is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  FrUCToSA is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with FrUCToSA.  If not, see <http://www.gnu.org/licenses/>.
+#
+#######################################################################
+
+import unittest
+
+
+class CreationOfGrafanaDashboardsTestCase(unittest.TestCase):
+    def test_executable_to_create_json_grafana_dashboards(self):
+        #  After starting the FrUCToSA system, Tux would like to connect
+        # it to Grafana. He finds out that there is an executable shipped
+        # with the package that can create an dashboard importable by
+        # Grafana. Great! Time to find out more about it:
+        self.fail("write me!")
