6500ada92234d38a1415d74afb21c02f71d77380
David Palao <david.palao@gmail.com>
2020-03-06 13:44:29 +0100

    WIP: plaholder to complete FT for make-fructosa-dashboard

--- tests/functional/test_grafana_dashboard.py
+++ tests/functional/test_grafana_dashboard.py
@@ -26,7 +26,9 @@ import functools
 
 from .utils import run_program
 
-from fructosa.constants import MAKE_DASHBOARD_PROGRAM, HOSTS_FILE_METAVAR
+from fructosa.constants import (
+    MAKE_DASHBOARD_PROGRAM, HOSTS_FILE_METAVAR, MAKE_DASHBOARD_HOSTS_HELP
+)
 
 
 make_fructosa_dashboard = functools.partial(run_program, MAKE_DASHBOARD_PROGRAM)
@@ -34,13 +36,43 @@ make_fructosa_dashboard = functools.partial(run_program, MAKE_DASHBOARD_PROGRAM)
 
 class CreationOfGrafanaDashboardsTestCase(unittest.TestCase):
     def test_executable_to_create_json_grafana_dashboards(self):
+        uphosts = HOSTS_FILE_METAVAR
+        hosts_help = MAKE_DASHBOARD_HOSTS_HELP
         #  After starting the FrUCToSA system, Tux would like to connect
         # it to Grafana. He finds out that there is an executable shipped
         # with the package that can create a dashboard importable by
         # Grafana. Great! Time to find out more about it:
-        with make_fructosa_dashboard("-h") as result_mk_fruct_dash:
-            uphosts = HOSTS_FILE_METAVAR
+        with make_fructosa_dashboard() as result_mk_fruct_dash:
+            
             self.assertIn(
                 f"the following arguments are required: {uphosts}",
-                result_mk_fruct_dash.stdout.decode()
+                result_mk_fruct_dash.stderr.decode()
             )
+        # Okay, okay. He tries the "-h" option:
+        with make_fructosa_dashboard("-h") as result_mk_fruct_dash:
+            # <streaming cl output>
+            joined_out = result_mk_fruct_dash.stdout.decode().replace("\n", " ")
+            while True:
+                new_joined = joined_out.replace("\t", " ")
+                new_new_joined = new_joined.replace("  ", " ")
+                if new_new_joined == joined_out:
+                    break
+                else:
+                    joined_out = new_new_joined
+            # </streaming cl output>
+            self.assertIn(
+                f"positional arguments: {uphosts} {hosts_help}",
+                joined_out
+            )
+            self.assertIn(
+                f"optional arguments: {} {}, {} {} {section_help}",
+                joined_out
+            )
+        # Fine, so he prepares a hosts file
+        #...
+        # and creates a dashboard from it:
+        #...
+        # it looks good. Let us try to validate it:
+        # ...
+        # Now he can import the created dashboard in Grafana and use it!
+            
