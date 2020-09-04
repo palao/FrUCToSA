50423a00270399657bd4e9ca072d18245238d117
David Palao <david.palao@gmail.com>
2020-09-04 13:46:29 +0200

    fix: setup.py and setup.cfg were inconsistent for bump2version

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ install_requirements = ["psutil"]
 
 setup(
     name="FrUCToSA",
-    version="version='0.3.1'",
+    version='0.3.1',
     description="A package to collect and analyze basic performance data from clusters",
     long_description=long_description,
     author="David Palao",
